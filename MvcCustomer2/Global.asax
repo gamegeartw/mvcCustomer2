dda0f97ecdd2b1c4b00193f579bca0ed2a61d5d1
gamegear <gamegear.tw@gmail.com>
2015-12-14 09:20:18 +0800

    First

---
+++ MvcCustomer2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCustomer2.MvcApplication" Language="C#" %>
---
+++ MvcCustomer2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcCustomer2.Startup))]
+namespace MvcCustomer2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
